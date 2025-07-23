70034c19b8ec669c889b54b62175638351942ca2
Signal <signal@kali>
2025-07-23 16:46:42 +0300

    Fix: Server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
