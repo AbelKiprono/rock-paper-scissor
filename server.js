120e48e5706fa8906f60c38eec9687f25fab949f
Signal <signal@kali>
2025-07-23 16:36:12 +0300

    Fix: Updated render port

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
